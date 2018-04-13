5279a9349d67ec80cffdd2e765d5045bb6b3b251
Chely Ho <adept_inept@yahoo.com>
2018-04-13 16:26:54 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    self = Artist.find_or_create_by(name: name)
   end
 end
