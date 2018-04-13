864011b6456ce9962065e799645ebb777e8142ea
Chely Ho <adept_inept@yahoo.com>
2018-04-13 16:38:52 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,9 +1,9 @@
 module ArtistsHelper
-  def display_artist(artist)
-    if artist.emtpy?
+  def display_artist(song)
+    if song.artist.emtpy?
       link_to "Add Artist", song_edit_path
     else
-      link_to artist.name, artist
+      link_to song.artist.name, song.artist
     end
   end
 end
