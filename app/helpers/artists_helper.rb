8b959a9c619b14ce7a55dd5256600ec3f4e30d40
Chely Ho <adept_inept@yahoo.com>
2018-04-13 16:41:21 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,9 +2,9 @@ module ArtistsHelper
   def display_artist(song)
     binding.pry
     if song.artist.present?
-      link_to "Add Artist", song_edit_path
-    else
       link_to song.artist.name, song.artist
+    else
+      link_to "Add Artist", song_edit_path
     end
   end
 end
